20edfd98032885914a031aa89fde5f07a6653675
Maria Rojas <alu0100605983@ull.edu.es>
2014-11-06 17:27:37 +0100

    Model -> añadiendo class Visit

--- model.rb
+++ model.rb
@@ -1,30 +1,35 @@
-class ShortenedUrl
-  include DataMapper::Resource
+class Shortenedurl
+	include DataMapper::Resource
+		property :id, Serial
+		property :url, Text
+		property :to, Text
+		property :id_usu, Text
 
-  property :id, Serial
-  property :url, Text
-  property :to, Text
-  property :id_usu, Text
-
-  #has n, :visits
+		has n, :visits
 end
 
-=begin
-class Visit 
+class Visit
 	include DataMapper::Resource
-
-	property :id,	Serial
-	property :created_at,	DateTime
-	property :ip,	IPAddress
-	property :country,	String
-	belongs_to :link
-
-	after :create, :set_country
+		property  :id,          Serial
+		property  :created_at,  DateTime
+		property  :ip,          IPAddress
+		property  :country,     Strin
+		#belongs_to  :link
+		belongs_to :shortenedur
+		after :create, :set_country
 
 	def set_country
-		xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip}"··
-    	self.country = XmlSimple.xml_in(xml.to_s, ...
-    	self.save
-  	end
+		xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip} #{self.ip}"
+		self.country = XmlSimple.xml_in(xml.to_s, { 'ForceArray' => false })['country'].to_s
+		self.save
+	end
+
+	def self.count_date(id)
+  		repository(:default).adapter.select("SELECT date(created_at) AS date, count(*) AS count FROM visits WHERE url_identifier = '#{id}' GROUP BY date(created_at)")
+	end
+
+	def self.count_country(id)
+		repository(:default).adapter.select("SELECT country, count(*) as count FROM visits where url_identifier = '#{id}' group by country")
+	end
   
-end =end 
+end
